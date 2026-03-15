c98c571d7f1fe4a0dfd818e7b59d3a342346ca33
Panenka29 <hflzyshang12@gmail.com>
2026-03-15 18:02:50 +0100

    Refactor Home component to use main tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Sopra FS26 – Group 05</h1>;
+  return (
+    <main style={{ background: "white", color: "black", padding: "40px" }}>
+      <h1>Sopra FS26 – Group 05</h1>
+    </main>
+  );
 }
