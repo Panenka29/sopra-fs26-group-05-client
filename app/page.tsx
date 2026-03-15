51ff60b1567570eb7d85419843f96fc1eb78629f
Panenka29 <hflzyshang12@gmail.com>
2026-03-15 18:00:26 +0100

    Refactor Home component to return only header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-"use client";
-
 export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontSize: "32px" }}>
-      <h1>Sopra FS26 – Group 05</h1>
-    </main>
-  );
+  return <h1>Sopra FS26 – Group 05</h1>;
 }
